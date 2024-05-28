8ed2cf62e78ee2abd4b086e4724f0c7e6cdfae05
Gregorius Abiyoso Kamandanu <kamandanug@gmail.com>
2024-05-28 15:30:29 +0700

    remove cart icon in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,12 +21,8 @@ store.getUserById()
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- <div class=" navbar" id="navbarSupportedContent"> -->
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li class="nav-item align-items-center">
-            <a class="nav-link" href="#">
-              <i class="bi bi-cart-check" style="font-size: 2rem"></i>
-            </a>
-          </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle align-items-center" href="#" role="button" data-bs-toggle="dropdown"
               aria-expanded="false">
